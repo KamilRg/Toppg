85d7f6f3a0c4db6be685b6b568807110d7fdeacc
KamilRg <97163663+KamilRg@users.noreply.github.com>
2022-06-29 10:56:54 +0200

    Add files via upload

---
+++ Infrastracture/Repositories/PostRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private static readonly ISet<Post> _posts = new HashSet<Post>()
+        {
+            new Post(1,"Even Heavier Heaven","This one is okay"),
+            new Post(2,"Thai Chicken Pizza","Favorite one"),
+            new Post(3,"Chicago Fire","It burns so good")
+        };
+        public IEnumerable<Post> GetAll()
+        {
+            return _posts;
+        }
+
+        public Post GetById(int id)
+        {
+            return _posts.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Post Add(Post post)
+        {
+            post.Id = _posts.Count() + 1;
+            post.Created = DateTime.UtcNow;
+            _posts.Add(post);
+            return post;
+        }
+
+        public void Update(Post post)
+        {
+            post.LastModified = DateTime.UtcNow;
+        }
+
+        public void Delete(Post post)
+        {
+            _posts.Remove(post);
+        }
+    }
+}
---
+++ PizzaApi/Controllers/PostsController.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace PizzaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostService _postService;
+        public PostsController(IPostService postService)
+        {
+            _postService = postService;
+        }
+        [SwaggerOperation(Summary = "Retrieves all posts")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var posts = _postService.getAllPosts();
+            return Ok(posts);
+        }
+        [SwaggerOperation(Summary = "Retrieves a specific post by unique id")]
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _postService.GetPostById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+    }
+}
